afe762cf6e991e9dbbb7c0f12a53da4829ae062d
Thanh Duy Ton <thaton@cisco.com>
2017-09-15 16:37:07 -0700

    First commit

---
+++ config.js
@@ -0,0 +1,4 @@
+window.devnetURL = 'https://devnet.cisco.com';
+window.dpURL = 'https://developer.cisco.com/';
+window.learningLabURL = 'https://learninglabs.cisco.com';
+window.authURL =  'https://auth-devnet.cisco.com/v1/auth/';
